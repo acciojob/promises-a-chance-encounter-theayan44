08487a81e667d5b03fcc7b120bc7d29cb2257c90
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-23 19:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const promises = [];
+for(let i=0; i<5; i++){
+	promises[i] = new Promise((res, rej)=>{
+		if(i % 2 == 0)
+			res(i+1);
+		else
+			rej(`Promise ${i+1} rejected with error`)
+	});
+}
+
+Promise.all(promises).then((values)=>{
+	const output = document.getElementById("output");
+	for(let i=0; i<values.length; i++){
+		const p = document.createElement("p");
+		p.innerText = values[i];
+		output.appendChild(p);
+	}
+})
